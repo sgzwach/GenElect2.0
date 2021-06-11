f11aab8efc66ab3780eaa4e5b2f0e6e73ee3715d
sgzwach <sgzwach@gmail.com>
2021-06-10 18:48:23 -0700

    Consolidated dynamic room items

---
+++ genElect/static/dynamicroom.js
@@ -0,0 +1,25 @@
+function checkRooms(event) {
+  if (event && event.target.id == 'room')
+    return
+  dict = $('form').serializeArray()
+  $.ajax({
+      type: 'POST',
+      url: '/api/rooms/offer',
+      data: JSON.stringify(dict), // or JSON.stringify ({name: 'jonas'}),
+      contentType: "application/json",
+      dataType: 'json'
+  }).done(function(data){
+    $('#room').empty();
+    $.each(data, function(k,v){
+      $('#room').append($("<option></option>").attr("value", v[0]).text(v[1]));
+    });
+  });
+}
+
+$('.form-control').change(function(event){
+  checkRooms(event);
+});
+
+$(document).ready(function() {
+  checkRooms(null);
+});
